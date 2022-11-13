d40a1c0107f3546ca80f6a5a4f8a9a7876c5cce6
IlkeHeyvaert <86114403+IlkeHeyvaert@users.noreply.github.com>
2022-11-13 20:27:12 +0100

    third commit

--- App.js
+++ App.js
@@ -1,38 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text style={styles.header}>Bookshop</Text>
       <Image style={styles.img} source={require('./assets/shopping_card.jpg')}/>
       <Text style={styles.shopping}>1</Text>
     <View>
-      <Text>Book title 1</Text>
-      <Text>Auteur</Text>
-      <Text>Lorem ipsum</Text>
-      <Text>ISBN</Text>
+      <Text style={styles.booktitle}>Fatale liefde</Text>
+      <Text>Carry Slee</Text>
+      <Text>Twee jonge vrouwen zitten in hun laatste jaar op de modeacademie. Ze willen samen een bedrijfje beginnen. Ze krijgen allebei problemen met hun vriendje.</Text>
+      <Text>ISBN 9789049924218</Text>
       <Button title="Read" color="black"/>
     </View>
     <View>
-      <Text>Book title 2</Text>
-      <Text>Auteur</Text>
-      <Text>Lorem ipsum</Text>
-      <Text>ISBN</Text>
+      <Image style={styles.img2} source={require('./assets/line.jpg')}/>
+    </View>
+    <View>
+      <Text>Voelen zonder filter : een plattegrond van het emotionele landschap</Text>
+      <Text>Fleur Van Groningen</Text>
+      <Text>Persoonlijk verslag van de auteur over hoe zij herstelde van traumatische gebeurtenissen uit haar jeugd en heeft leren omgaan met ingewikkelde gevoelens. Met tips voor het reflecteren op en reguleren van complexe emoties.</Text>
+      <Text>ISBN 9789463938372</Text>
       <Button title="Read" color="black"/>
     </View>
     <View>
-      <Text>Book title 3</Text>
-      <Text>Auteur</Text>
-      <Text>Lorem ipsum</Text>
-      <Text>ISBN</Text>
+      <Image style={styles.img2} source={require('./assets/line.jpg')}/>
+    </View>
+    <View>
+      <Text>Reminders of him</Text>
+      <Text>Colleen Hoover</Text>
+      <Text>Een vrouw hoopt na een tragische fout en een gevangenisstraf op hereniging met haar jonge dochter. Haar familie houdt haar op afstand en er ontwikkelt zich een risicovolle relatie met een man uit haar verleden.</Text>
+      <Text>ISBN 9789020548648</Text>
       <Button title="Read" color="black"/>
     </View>
     <View>
-      <Text>Book title 4</Text>
-      <Text>Auteur</Text>
-      <Text>Lorem ipsum</Text>
-      <Text>ISBN</Text>
+      <Image style={styles.img2} source={require('./assets/line.jpg')}/>
+    </View>
+    <View>
+      <Text>Vrede op aarde</Text>
+      <Text>Paul Dini</Text>
+      <Text>Superman richt zich op het oplossen van het wereldvoedselprobleem. Kolomstrip in kleur. Vanaf 15 jaar.</Text>
+      <Text>ISBN 9789464600032</Text>
       <Button title="Read" color="black"/>
     </View>
       
@@ -40,7 +49,7 @@ export default function App() {
       
       
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
@@ -53,6 +62,7 @@ const styles = StyleSheet.create({
     fontSize: 30,
     margin: 30,
     marginTop: 40,
+    fontWeight: 'bold',
   },
   img: {
     width: '10%',
@@ -62,5 +72,11 @@ const styles = StyleSheet.create({
   },
   shopping: {
     fontSize: 20,
+    marginLeft: 360,
+    marginTop: -15,
+  },
+  booktitle: {
+    fontSize: 20,
+
   }
 });
