a4f98e2c804006e260b0dc012af48d574debb41e
IlkeHeyvaert <86114403+IlkeHeyvaert@users.noreply.github.com>
2022-11-13 17:48:03 +0100

    second commit

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>Bookshop</Text>
+      <Image style={styles.img} source={require('./assets/shopping_card.jpg')}/>
+      <Text style={styles.shopping}>1</Text>
+    <View>
+      <Text>Book title 1</Text>
+      <Text>Auteur</Text>
+      <Text>Lorem ipsum</Text>
+      <Text>ISBN</Text>
+      <Button title="Read" color="black"/>
+    </View>
+    <View>
+      <Text>Book title 2</Text>
+      <Text>Auteur</Text>
+      <Text>Lorem ipsum</Text>
+      <Text>ISBN</Text>
+      <Button title="Read" color="black"/>
+    </View>
+    <View>
+      <Text>Book title 3</Text>
+      <Text>Auteur</Text>
+      <Text>Lorem ipsum</Text>
+      <Text>ISBN</Text>
+      <Button title="Read" color="black"/>
+    </View>
+    <View>
+      <Text>Book title 4</Text>
+      <Text>Auteur</Text>
+      <Text>Lorem ipsum</Text>
+      <Text>ISBN</Text>
+      <Button title="Read" color="black"/>
+    </View>
+      
+      
+      
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +46,21 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  header: {
+    fontSize: 30,
+    margin: 30,
+    marginTop: 40,
+  },
+  img: {
+    width: '10%',
+    height: 45,
+    marginTop: -65,
+    marginLeft: 320,
+  },
+  shopping: {
+    fontSize: 20,
+  }
 });
